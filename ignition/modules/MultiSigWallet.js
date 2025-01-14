a987037133dea82fbca9249b164dc72fae2252c0
Divya <divyabaheti101@gmail.com>
2025-01-14 20:27:15 +0530

    deploy script update... incorrect

--- ignition/modules/MultiSigWallet.js
+++ ignition/modules/MultiSigWallet.js
@@ -2,10 +2,14 @@
 // Learn more about it at https://hardhat.org/ignition
 
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+const { ethers } = require('hardhat');
+
+const [owner1, owner2, owner3] = await ethers.getSigners();
+const qourum = 2;
 
 module.exports = buildModule("MultiSigWalletModule", (m) => {
   
-  const multiSigWallet = m.contract("MultiSigWallet");
+  const multiSigWallet = m.contract("MultiSigWallet", [owner1.address, owner2.address, owner3.address], qourum);
 
   return { multiSigWallet };
 });
