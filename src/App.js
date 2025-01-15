3fa7f9d699e1d42f18e3a4dab0d6baf15cbcbad7
Divya <divyabaheti101@gmail.com>
2025-01-15 17:22:19 +0530

    wagmi seteup index.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 
