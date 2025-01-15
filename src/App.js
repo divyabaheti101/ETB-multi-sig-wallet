1c24dce224b759b485a17fc6c4cf295dc462c56a
Divya <divyabaheti101@gmail.com>
2025-01-15 17:38:11 +0530

    Wagmi setup, and ConnectButton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 function App() {
 
   const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
   return (
-    
+    <div className='container flex flex-col items-center mt-10'>
+      <div className='flex-mb-6'>
+        <ConnectButton />
+      </div>
+    </div>
   );
 }
 
